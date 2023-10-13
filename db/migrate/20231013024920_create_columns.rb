1e127bcc16c4485ffa3a72c827145d9875f56b8b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-10-13 11:57:49 +0900

    マイグレーションの追加

---
+++ db/migrate/20231013024920_create_columns.rb
@@ -0,0 +1,12 @@
+class CreateColumns < ActiveRecord::Migration[7.0]
+  def change
+    create_table :columns do |t|
+      t.references :table, null: false, foreign_key: true, comment: 'テーブルID' 
+      t.string :name, null: false, comment: 'カラム名'
+
+      t.timestamps
+    end
+
+    add_index :columns, [:table_id, :name], unique: true
+  end
+end
