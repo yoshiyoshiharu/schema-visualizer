6d9384605eba0f9fa127759669c97e4f5ed0d986
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-10-18 21:19:08 +0900

    雛形作成

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
