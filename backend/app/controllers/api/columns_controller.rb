d849e6460a2f05a175ba25b1b490e241d5477beb
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-10-22 13:13:50 +0900

    refactor error handling

--- backend/app/controllers/api/columns_controller.rb
+++ backend/app/controllers/api/columns_controller.rb
@@ -2,13 +2,8 @@
 
 class Api::ColumnsController < ApplicationController
   def index
-    table = Table.find_by(id: params[:table_id])
+    table = Table.find(params[:table_id])
 
-    if table.nil?
-      return render json: { error: '404 error' }, status: 404
-    end
-
-    columns = table.columns
-    render json: columns
+    render json: table.columns
   end
 end
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :error404
+
+  def error404
+    render json: { error: '404 error' }, status: 404
+  end
 end
