c85ca106a935fbc079cabcefe78f379d31ccafeb
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-10-24 17:35:47 +0900

    カラム一覧を表示する

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,25 +1,35 @@
 import { Product } from '../types/product'
 import SidebarTables from './Sidebar/Tables'
+import SidebarColumns from './Sidebar/Columns'
 import { useEffect, useState } from 'react'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
 
 export default function Sidebar() {
-  const [products, setProducts] = useState<Product[]>([])
+  const [productsWithTables, setProductsWithTables] = useState<Product[]>([])
+  const [productsWithColumns, setProductsWithColumns] = useState<Product[]>([])
 
-  const fetchProducts = async () => {
+  const fetchProductsWithTables = async () => {
     const res = await fetch(`${BASE_URL}/api/products/tables`)
     const data = await res.json()
-    setProducts(data)
+    setProductsWithTables(data)
+  }
+
+  const fetchProductsWithColumns = async () => {
+    const res = await fetch(`${BASE_URL}/api/products/columns?name_like=article`)
+    const data = await res.json()
+    setProductsWithColumns(data)
   }
 
   useEffect(() => {
-    fetchProducts()
+    fetchProductsWithTables()
+    fetchProductsWithColumns()
   }, [])
 
   return (
     <aside className="w-1/4 overflow-auto h-full bg-gray-50">
-      <SidebarTables products={products} />
+      <SidebarTables products={productsWithTables} />
+      <SidebarColumns products={productsWithColumns} />
     </aside>
   )
 }
--- frontend/components/Sidebar/Columns.tsx
+++ frontend/components/Sidebar/Columns.tsx
@@ -12,7 +12,7 @@ import {
   Box
 } from '@chakra-ui/react'
 
-export default function SidebarColumns({ columns }: { columns: Column[] }) {
+export default function SidebarColumns({ products }: { products: Product[] }) {
   const setTargetTable = useSetRecoilState(targetTableState)
 
   const handleClick = (product: Product, table: Table) => () => {
@@ -26,6 +26,33 @@ export default function SidebarColumns({ columns }: { columns: Column[] }) {
     <>
       <h2 className="font-bold p-4">Columns</h2>
       <Accordion allowMultiple>
+        {products.map((product) => (
+          <AccordionItem key={product.id}>
+            <h2>
+              <AccordionButton>
+                <Box>
+                  {product.name}({product.tables.map((table) => table.columns).length})
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <ul className="ml-4">
+                {product.tables.map((table) => (
+                  table.columns.map((column) => (
+                    <li
+                      key={table.id}
+                      onClick={handleClick(product, table)}
+                      className="cursor-pointer hover:text-gray-500"
+                    >
+                      {table.name}.{column.name}
+                    </li>
+                  )))
+              )}
+            </ul>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
       </Accordion>
     </>
   )
