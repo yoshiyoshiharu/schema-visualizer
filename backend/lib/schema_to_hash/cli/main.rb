09cda4cfc7dfb96620b95b59fc22264092913929
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-11-11 17:24:53 +0900

    refactor dir structure

--- backend/lib/schema_to_hash/cli/main.rb
+++ backend/lib/schema_to_hash/cli/main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'db'
-require_relative 'fetcher'
+require_relative '../db'
+require_relative '../lib/fetcher'
 
 if __FILE__ == $PROGRAM_NAME
   db = Db.connect
