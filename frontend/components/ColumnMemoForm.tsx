87b0ef9d817275ceceff1c6f5d2a9510944fa1c1
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-11-26 14:16:12 +0900

    add textarea

---
+++ frontend/components/ColumnMemoForm.tsx
@@ -0,0 +1,14 @@
+import {
+  Textarea,
+} from '@chakra-ui/react'
+
+export default function ColumnMemoForm() {
+  return (
+    <div>
+      <Textarea
+        size='xs'
+        rows={2}
+      />
+    </div>
+  )
+}
