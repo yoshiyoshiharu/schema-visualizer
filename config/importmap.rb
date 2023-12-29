49c482d77c4086fd374f396da8a1c7901956564b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-12-29 16:28:01 +0900

    bootstrapの導入

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", preload: true
