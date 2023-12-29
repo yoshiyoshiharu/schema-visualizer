1a916e7418b86b54700cc6de2dfc2b6bcba6b274
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-12-29 17:03:59 +0900

    importmap bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,10 @@
 
 pin "application", preload: true
 
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", preload: true
+pin "bootstrap",
+    to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js",
+    preload: true
+
+pin "@popperjs/core",
+    to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js",
+    preload: true
