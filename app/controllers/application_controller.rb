e298828fc8c3c9381aeb27a82a0aabf43565e55b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-12-29 16:40:00 +0900

    fix controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :error404
 
   def error404
