524272417878e2186be4789b58f2c823394b932a
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-12-29 16:48:10 +0900

    delete unec application controller codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :error404
-
-  def error404
-    render json: { error: '404 error' }, status: :not_found
-  end
 end
