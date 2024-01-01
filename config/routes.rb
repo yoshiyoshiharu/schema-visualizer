da0806d3e669c5844f121d57830b4b240fcbe404
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-01 15:46:52 +0900

    add memo form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resource :home, only: %w(index)
 
   resources :tables, only: %w(index show)
+
+  resources :columns, only: %w() do
+    resource :memo, only: %w(update)
+  end
 end
