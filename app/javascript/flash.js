899ef6cbf3020a555a7b14db4de1e617f7539850
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-02 12:43:35 +0900

    クラスの削除によって制御する

--- app/javascript/flash.js
+++ app/javascript/flash.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbo:submit-end', function() {
-  setTimeout(function () {
-    document.getElementById("alert").style.display = "none";
+  setTimeout(function() {
+    document.getElementById("alert").classList.remove('show');
   }, 2000);
 });
