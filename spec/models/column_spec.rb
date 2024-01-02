a116c41fa0fe570fa97bf11cdbd372ab375bbc7c
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-02 22:53:25 +0900

    fix rubocop of rspec

--- spec/models/column_spec.rb
+++ spec/models/column_spec.rb
@@ -2,18 +2,13 @@
 
 RSpec.describe Column do
   describe '#name_like' do
-    before do
-      create(:product) do |product|
-        create(:table, product:) do |table|
-          @column1 = create(:column, table:, name: 'id')
-          @column2 = create(:column, table:, name: 'user_id')
-          @column3 = create(:column, table:, name: 'name')
-        end
-      end
-    end
+    let!(:table) { create(:table) }
+    let!(:column1) { create(:column, table: table, name: 'id') }
+    let!(:column2) { create(:column, table: table, name: 'user_id') }
+    let!(:column3) { create(:column, table: table, name: 'name') }
 
     it 'キーワードに一致するレコードを返す' do
-      expect(described_class.name_like('id')).to eq [@column1, @column2]
+      expect(described_class.name_like('id')).to eq [column1, column2]
     end
 
     context 'すべてのレコードの名前にキーワードが含まれないとき' do
