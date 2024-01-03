adf37d2c9e49e5c36fec7c92ea7c5c02c6f59049
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-03 19:27:57 +0900

    fix fix.sh

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-docker compose run --rm web bash rubocop -A
+docker compose run --rm web bundle exec rubocop -A
 docker compose run --rm web bash -c '
   shopt -s dotglob globstar nullglob && bundle exec htmlbeautifier **/*.html.erb
 '
