8094be8725aea520b42ee93c2ae9d16f8b2ef830
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-03 19:08:46 +0900

    exit_statusを設定する

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
 
-docker compose run --rm web bundle exec rubocop
-docker compose run --rm web bundle exec erblint --lint-all
+docker compose run --rm web bundle exec rubocop || exit_status=$?
+docker compose run --rm web bundle exec erblint --lint-all || exit_status=$?
 docker compose run --rm web bash -c '
   shopt -s dotglob globstar nullglob && bundle exec htmlbeautifier -l **/*.html.erb
-'
-docker compose run --rm web bundle exec brakeman -Aqw1 --no-pager
+' || exit_status=$?
+docker compose run --rm web bundle exec brakeman -Aqw1 --no-pager || exit_status=$?
+
+exit ${exit_status:-0}
