4aef51bf44e2c8960d70ddbd601bb7dd0af1411e
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-03 19:15:10 +0900

    #!/bin/bash -euではなくset -euにする

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -eu
+#!/bin/bash
 
-docker compose run --rm web bundle exec rubocop || exit_status=$?
-docker compose run --rm web bundle exec erblint --lint-all || exit_status=$?
+set -eu
+
+docker compose run --rm web bundle exec rubocop
+docker compose run --rm web bundle exec erblint --lint-all
 docker compose run --rm web bash -c '
   shopt -s dotglob globstar nullglob && bundle exec htmlbeautifier -l **/*.html.erb
-' || exit_status=$?
-docker compose run --rm web bundle exec brakeman -Aqw1 --no-pager || exit_status=$?
-
-exit ${exit_status:-0}
+'
+docker compose run --rm web bundle exec brakeman -Aqw1 --no-pager
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
-#!/bin/bash -eu
+#!/bin/bash
+
+set -eu
 
 docker compose run --rm web bundle exec rspec
