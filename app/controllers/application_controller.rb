05799c6e618f4af3ed09bda9661ac5f4b4c1506d
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-04 19:02:31 +0900

    ログイン機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  before_action :require_login
+
+  private
+
+  def require_login
+    return if current_user.present?
+
+    redirect_to new_sessions_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  def self.find_from_auth_hash(auth_hash)
+    User.find_by(email: user_params_from_auth_hash(auth_hash)[:email])
+  end
+
+  def self.user_params_from_auth_hash(auth_hash)
+    {
+      email: auth_hash.info.email
+    }
+  end
+
+  private_class_method :user_params_from_auth_hash
 end
