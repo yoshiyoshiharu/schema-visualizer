d83c36908381c389d2989cc7640f769522fe5532
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-05 22:20:57 +0900

    add tables controller spec

---
+++ spec/requests/tables_controller_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+RSpec.describe TablesController do
+  include LoginSupport
+
+  describe '#show' do
+    let!(:table) { create(:table) }
+
+    before do
+      login_as(create(:user))
+    end
+
+    context '通常のリクエストのとき' do
+      before do
+        get table_path(table)
+      end
+
+      it 'ステータスコード200を返すこと' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'SidebarのHTMLも返すこと' do
+        assert_select 'aside'
+      end
+    end
+
+    context 'Turboリクエストのとき' do
+      before do
+        get table_path(table), headers: { 'Turbo-Frame': 'table' }
+      end
+
+      it 'ステータスコード200を返すこと' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'SidebarのHTMLは返さないこと' do
+        assert_select 'aside', false
+      end
+    end
+  end
+end
