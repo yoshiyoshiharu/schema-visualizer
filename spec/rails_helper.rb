b3ccecd97687566a554873f44a5ee97aebd94ad0
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-05 17:48:25 +0900

    add login support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,11 +6,15 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
+Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+OmniAuth.config.test_mode = true
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
