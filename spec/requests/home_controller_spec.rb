8fcfa24570ae50c25b99bc5ba7999135288d3e00
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-05 18:06:00 +0900

    add home controller spec

---
+++ spec/requests/home_controller_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+RSpec.describe HomeController do
+  include LoginSupport
+
+  describe '#index' do
+    before do
+      login_as(create(:user))
+    end
+
+    context '通常のリクエストのとき' do
+      before do
+        get root_path
+      end
+
+      it 'ステータスコード200を返すこと' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'SidebarのHTMLも返すこと' do
+        assert_select 'aside'
+      end
+    end
+
+    context 'Turboリクエストのとき' do
+      before do
+        get root_path, headers: { 'Turbo-Frame': 'table' }
+      end
+
+      it 'ステータスコード200を返すこと' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'SidebarのHTMLは返さないこと' do
+        assert_select 'aside', false
+      end
+    end
+  end
+end
