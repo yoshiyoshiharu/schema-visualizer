8b0ed95b2b580a1c388535ca23511d7553403a11
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-05 15:10:24 +0900

    auto correct erb lint

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -3,6 +3,7 @@
 set -eu
 
 docker compose run --rm web bundle exec rubocop -A
+docker compose run --rm web bundle exec erblint --lint-all -a
 docker compose run --rm web bash -c '
   shopt -s dotglob globstar nullglob && bundle exec htmlbeautifier **/*.html.erb
 '
