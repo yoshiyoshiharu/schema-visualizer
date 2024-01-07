74116d88b75182534d77089de16bb89c962188c8
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-07 22:06:15 +0900

    fix column memos controlle spec

--- spec/requests/column_memos_controller_spec.rb
+++ spec/requests/column_memos_controller_spec.rb
@@ -55,9 +55,9 @@ RSpec.describe ColumnMemosController do
     end
   end
 
-  # update は TurboStremaリクエストなので修正する
   describe '#update' do
     let!(:column) { create(:column) }
+    let!(:column_memo) { create(:column_memo, column:) }
 
     context '通常のリクエストのとき' do
       it 'ステータスコード404を返すこと' do
@@ -66,27 +66,28 @@ RSpec.describe ColumnMemosController do
       end
     end
 
-    context 'Turboリクエストのとき' do
-      context '正常な値を送信したとき' do
-        let(:valid_param) do
-          {
-            column_memo: {
-              content: 'Updated memo'
-            }
+    context 'Turboリクエストで正常な値を送信したとき' do
+      let(:valid_param) do
+        {
+          column_memo: {
+            content: 'Updated memo'
           }
-        end
+        }
+      end
 
-        it 'showにリダイレクトすること' do
-          patch column_memo_path(column), params: valid_param, headers: { 'Turbo-Frame': "column_#{column.id}" }
-          pp response.body
-          expect(response).to redirect_to(column_memo_path(column))
-        end
+      it 'ステータスコード200を返すこと' do
+        patch column_memo_path(column),
+              params: valid_param,
+              headers: { 'Turbo-Frame': "column_#{column.id}", Accept: 'text/vnd.turbo-stream.html' }
+        expect(response).to have_http_status(:ok)
+      end
 
-        it 'メモが更新されること' do
-          expect do
-            patch column_memo_path(column), params: valid_param, headers: { 'Turbo-Frame': "column_#{column.id}" }
-          end.to change { column.reload.memo.content }.to('Updated memo')
-        end
+      it 'メモが更新されること' do
+        expect do
+          patch column_memo_path(column),
+                params: valid_param,
+                headers: { 'Turbo-Frame': "column_#{column.id}", Accept: 'text/vnd.turbo-stream.html' }
+        end.to change { column_memo.reload.content }.to('Updated memo')
       end
     end
   end
