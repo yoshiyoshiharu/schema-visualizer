57af1c49e439792c4271be85f350ec37e05448ee
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-01-07 17:23:51 +0900

    add users controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,4 +5,9 @@ FactoryBot.define do
     name { 'テストユーザー' }
     email { 'haruki.osaka.u@gmail.com' }
   end
+
+  factory :login_user, class: 'User' do
+    name { 'ログインユーザー' }
+    email { 'login_user@test.com' }
+  end
 end
