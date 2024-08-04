002aad97317ba0e9be81cedb861d3813b98dafc3
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-08-04 20:54:55 +0900

    fix setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,4 @@ set -eu
 
 docker compose build
 docker compose run --rm web bundle exec rails db:drop db:create db:migrate db:seed
+docker compose run --rm web bundle exec rails db:drop db:create db:migrate RAILS_ENV=test
